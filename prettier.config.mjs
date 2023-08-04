553aa2bf87e97cf051a905b772e023cc2f2aed0c
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-08-04 07:14:09 +0000

    add prettier settings

---
+++ prettier.config.mjs
@@ -0,0 +1,39 @@
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+// /** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+
+// /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  // pluginSearchDirs: false,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    // "prettier-plugin-tailwindcss",
+    "prettier-plugin-packagejson",
+  ],
+  // tailwindConfig: "./tailwind.config.js",
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/(.*)$",
+    "^~/utils/(.*)$",
+    "^~/components/(.*)$",
+    "^~/styles/(.*)$",
+    "^~/(.*)$",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.9.5",
+};
+
+export default config;
