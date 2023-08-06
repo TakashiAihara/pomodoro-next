5e3ee55bc65c63c3819557e393aa4f2a8a0e1f5f
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-08-06 14:54:41 +0000

    adjust delay for fixed failed storybook render

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -9,7 +9,7 @@ export const decorators = [withScreenshot];
 export const screenshotOptions: { screenshot: ScreenshotOptions } = {
   screenshot: {
     viewports: ["iPhone 11", "iPad", "1024x768"],
-    delay: 200,
+    delay: 400,
     fullPage: true,
   },
 };
