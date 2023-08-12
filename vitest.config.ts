5437000a1a890c20924eef44f05b2e47e70a0f60
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-08-12 10:58:10 +0000

    Summarize test reports into test-results

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
-      reportsDirectory: "./test-results/unit/coverage",
+      reportsDirectory: "./test-results/unit",
     },
   },
   esbuild: {
