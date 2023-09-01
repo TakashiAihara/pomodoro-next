96a568da3eadbd2a9742a1e21b5c5f0fe75ff761
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-09-01 14:47:48 +0000

    refactoring playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,102 +1,82 @@
-import {
-  defineConfig,
-  devices,
-  type PlaywrightTestConfig,
-} from "@playwright/test";
+import { defineConfig, devices } from "@playwright/test";
 
-const defaultConfig: PlaywrightTestConfig = {
+const defaultDevices = [
+  {
+    name: "chromium",
+    use: { ...devices["Desktop Chrome"] },
+  },
+  {
+    name: "firefox",
+    use: { ...devices["Desktop Firefox"] },
+  },
+  {
+    name: "webkit",
+    use: { ...devices["Desktop Safari"] },
+  },
+  {
+    name: "Mobile Chrome",
+    use: { ...devices["Pixel 5"] },
+  },
+  {
+    name: "Mobile Safari",
+    use: { ...devices["iPhone 12"] },
+  },
+];
+
+const defaultWebServer = {
+  command: "BROWSER=none npm run start",
+  url: "http://localhost:3000",
+  reuseExistingServer: true,
+};
+
+const defaultConfig = {
   testDir: "./test/e2e",
+  outputDir: "./__e2e__/",
+  projects: defaultDevices,
+};
+
+const localConfig = defineConfig({
+  ...defaultConfig,
+  webServer: defaultWebServer,
   fullyParallel: true,
   forbidOnly: false,
   retries: 0,
   reporter: "html",
   use: {
-    trace: "on-first-retry",
     headless: true,
-    video: "retry-with-video",
     screenshot: "only-on-failure",
+    video: "retry-with-video",
+    trace: "on-first-retry",
   },
-  webServer: {
-    command: "BROWSER=none npm run start",
-    url: "http://localhost:3000",
-    reuseExistingServer: true,
-    stdout: "ignore",
-    stderr: "pipe",
-  },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
-  ],
-};
+});
 
-const ciConfig: PlaywrightTestConfig = {
-  testDir: `./test/e2e`,
-  outputDir: "./__e2e__/",
+const ciConfig = defineConfig({
+  ...defaultConfig,
   fullyParallel: true,
   forbidOnly: true,
   retries: 1,
-  workers: 2,
+  workers: 1,
   webServer: {
-    command: "BROWSER=none npm run start",
-    url: "http://localhost:3000",
-    reuseExistingServer: true,
-    stdout: "ignore",
-    stderr: "pipe",
+    ...defaultWebServer,
+    reuseExistingServer: false,
   },
   reporter: [
-    ["html", { open: "never", outputFolder: "./test-results/e2e" }],
-    ["github", { open: "never", outputFolder: "./test-results/e2e" }],
+    [
+      "blob",
+      {
+        open: "never",
+        outputDir: "./test-results/e2e_blob",
+      },
+    ],
   ],
   use: {
     headless: true,
-    screenshot: "on",
-    video: "on",
-    trace: "on",
+    screenshot: "only-on-failure",
+    video: "retry-with-video",
+    trace: "retry-with-trace",
     navigationTimeout: 3000,
     actionTimeout: 3000,
   },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-    {
-      name: "Mobile Chrome",
-      use: { ...devices["Pixel 5"] },
-    },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPhone 12"] },
-    },
-  ],
-};
+});
 
-export default defineConfig(
-  process.env.CI !== undefined ? ciConfig : defaultConfig,
-);
+export default process.env.CI !== undefined ? ciConfig : localConfig;
